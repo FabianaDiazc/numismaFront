8b89abd5df1407b3d8601ffd4ec6569c0ba3f690
Fabiana Diaz <fabiana.diaz@javeriana.edu.co>
2017-05-23 11:36:02 -0500

    arreglando errores footer

--- src/app/services/config.ts
+++ src/app/services/config.ts
@@ -1,2 +1,2 @@
-// export let SERVER_URL: string = 'http://45.55.66.114';
-export let SERVER_URL: string = 'http://localhost:8000';
+export let SERVER_URL: string = 'http://45.55.66.114';
+// export let SERVER_URL: string = 'http://localhost:8000';
